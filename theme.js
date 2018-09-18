c901944bfc97363719334f76b690d41fe268de09
Ben John <github@benjohn.de>
2018-09-18 13:18:48 +0200

    Finalized slides

--- theme.js
+++ theme.js
@@ -2,12 +2,6 @@ import theme from 'mdx-deck/themes'
 
 export default {
   ...theme,
-
-  // Customize your presentation theme here.
-  //
-  // Read the docs for more info:
-  // https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md
-  // https://github.com/jxnblk/mdx-deck/blob/master/docs/themes.md
   font: 'Asap, sans-serif',
   colors: {
     text: '#5B5B5B',
