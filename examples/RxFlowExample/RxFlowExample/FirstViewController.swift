d8ef23d56c98e3e490d89b6b0e2caee0bed40a57
Ben John <github@benjohn.de>
2018-09-19 13:27:35 +0200

    Add stub for RxFlowExample

---
+++ examples/RxFlowExample/RxFlowExample/FirstViewController.swift
@@ -0,0 +1,22 @@
+// FirstViewController.swift
+//
+// - Authors:
+// Ben John
+//
+// - Date: 19.09.18
+//
+// Copyright © 2018 Ben John. All rights reserved.
+    
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ examples/RxFlowExample/RxFlowExample/SecondViewController.swift
@@ -0,0 +1,22 @@
+// SecondViewController.swift
+//
+// - Authors:
+// Ben John
+//
+// - Date: 19.09.18
+//
+// Copyright © 2018 Ben John. All rights reserved.
+    
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
