af288b65036ea8ebc5cc53ee3a309ad2af1326e6
Ben John <github@benjohn.de>
2018-09-19 13:35:43 +0200

    Adding dependencies with Carthage

---
+++ examples/RxFlowExample/Carthage/Checkouts/RxFlow/RxFlowDemo/RxFlowDemo/Flows/DemoStep.swift
@@ -0,0 +1,34 @@
+//
+//  DemoStep.swift
+//  RxFlowDemo
+//
+//  Created by Thibault Wittemberg on 17-07-25.
+//  Copyright (c) RxSwiftCommunity. All rights reserved.
+//
+
+import RxFlow
+
+// swiftlint:disable identifier_name
+enum DemoStep: Step {
+    // Onboarding
+    case onboarding
+    case apiKey
+    case apiKeyIsComplete
+    case login
+    case loginIsComplete
+    case onboardingIsComplete
+
+    // Dashboard
+    case dashboard
+    case movieList
+
+    // Movies
+    case moviePicked (withMovieId: Int)
+    case castPicked (withCastId: Int)
+
+    // Settings
+    case settings
+    case about
+    case settingsIsComplete
+}
+// swiftlint:enable identifier_name
